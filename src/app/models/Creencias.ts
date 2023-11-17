bc0ed2e3315b8f6cb347109e29f635680c811984
Hiram weyman <hiram.wv@gmail.com>
2023-11-17 14:47:50 -0600

    Se termino la funcion de archivos
    Se termino la funcion de archivos

---
+++ src/app/models/Creencias.ts
@@ -0,0 +1,17 @@
+export class Creencias
+{
+       creencia_id :number;
+       creencia_irra1 :string;
+       creencia_irra2 :string;
+       creencia_irra3 :string;
+       creencia_irra4 :string;
+       creencia_irra5 :string;
+       creencia_irra6 :string;
+       creencia_irra7 :string;
+       creencia_irra8 :string;
+       creencia_irra9 :string;
+       creencia_irra10 :string;
+       creencia_fecha_captura :Date;
+       creencia_fecha_modificacion :Date;
+       creencia_paciente_id :number;
+}
